feb77d8ff7c31cf7532c7d5bd0c3a93977fdbf30
TicklemeKrystle <krystle.akuffo@gmail.com>
2022-03-11 15:14:22 -0800

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - function that prints all single digit starting
- *   * from 0 followed by a new line
- *    *
- *     * Return: Always 0 (Success)
- *      */
+ * main - function that prints all single digit starting
+ * from 0 then a new line
+ *
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
