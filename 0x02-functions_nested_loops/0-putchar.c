a196dd76deadad6f172c33e627e501e48cc1f47f
TicklemeKrystle <krystle.akuffo@gmail.com>
2022-03-15 14:13:17 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  * main - This function prints out a string followed
+ *   * by a new line
+ *    *
+ *     * Description: you are not allowed to use standard libraries
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+
+int main(void)
+{
+		char p[] = "_putchar";
+			int i;
+
+				for (i = 0; i < 8; i++)
+						{
+									_putchar(p[i]);
+										}
+
+					_putchar('\n');
+						return (0);
+}
