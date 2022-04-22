653811244b48e8369dde3e787327d10d10759c12
TicklemeKrystle <krystle.akuffo@gmail.com>
2022-04-21 22:33:32 -0700

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * list_len - return number of elements
+ * @h: list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
+
+© 2022 GitHub, Inc.
+
