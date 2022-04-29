3780f101a3cde3bd21a7b980a34195ffe51cd1f7
TicklemeKrystle <krystle.akuffo@gmail.com>
2022-04-28 22:53:18 -0700

    100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endiness of system
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
